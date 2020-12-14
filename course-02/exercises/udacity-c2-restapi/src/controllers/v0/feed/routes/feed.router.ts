4f46f1fb900aa48d8e37cd12f135b6e8cea2a1c1
Furkan Ağaçayaklar <irfan.furkan.agacayaklar@ipt.fraunhofer.de>
2020-12-14 22:14:02 +0100

    modified restapi

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,38 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async(req: Request, res: Response) => {
+    let { id } = req.params;
+    const item = await FeedItem.findByPk(id);
+    item.url = AWS.getGetSignedUrl(item.url);
+    res.send(item);
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        let { id } = req.params;
+        const {caption , url} = req.body;
+        
+        if (!caption && !url) {
+            return res.status(400).send({ message: 'Data is not provided' });
+        }
+
+        const item = await FeedItem.findByPk(id);
+
+        if (caption) {
+            item.caption = caption
+        }
+        if (url) {
+            item.url = url
+        }
+
+        const saved_item = await item.save();
+    
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        res.status(201).send(saved_item);
 });
 
 
